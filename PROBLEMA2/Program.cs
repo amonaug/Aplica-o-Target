868ef024a82de20ab50c8064ca9100d21fb9a9b5
amon <amonaug@gmail.com>
2024-09-06 20:41:16 -0300

    Primeiro Commit

---
+++ PROBLEMA2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problema_2
+{
+    class Progam
+    {
+        static void Main()
+        {
+            string texto = "A aranha arranha a jarra e a jarra arranha a aranha, nem a aranha arranha a jarra nem a jarra arranha a aranha";
+
+            int cont;
+            cont = LetraA(texto);
+
+            if(cont == 0)
+                Console.WriteLine("Não existe nenhuma letra 'a' no texto");
+            else
+                Console.WriteLine("Existem {0} letras 'a' no texto", cont);
+        }
+
+        static int LetraA(string textoP)
+        {
+            int cont = 0;
+            textoP = textoP.ToLower();
+
+            for(int i = 0; i < textoP.Length; i++)
+            {
+                if(textoP[i] == 'a')
+                    cont++;
+            }
+
+            return cont;
+        }
+    }
+}
---
+++ Problema1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Problema_1
+{
+    class Progam
+    {
+        static void Main()
+        {
+            int num;
+            bool fibonacci;
+
+            Console.Write("Digite o número a ser testado: ");
+            num = int.Parse(Console.ReadLine());
+            fibonacci = Teste(num);
+
+            if(fibonacci == true)
+                Console.WriteLine("O número {0} faz parte da sequência de fibonacci", num);
+            else
+                Console.WriteLine("{0} não pertence a sequência de fibonacci", num);
+        }
+
+        static bool Teste(int numP)
+        {
+            double caso_1, caso_2;
+
+            //Expressão para calcular se o número faz parte da sequência de fibonacci
+            caso_1 = (5*Math.Pow(numP, 2)) + 4;
+            caso_2 = (5*Math.Pow(numP, 2)) - 4;
+
+            //Descobrindo se a raiz é exata
+            if(Math.Sqrt(caso_1) == (int)Math.Sqrt(caso_1) || Math.Sqrt(caso_2) == (int)Math.Sqrt(caso_2))
+                return true;
+            else
+                return false;
+        }
+    }
+}
